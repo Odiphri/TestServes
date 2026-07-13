984150134d223d0fcda146e2f347c52767c9c8fa
Odiphri <odiphrimarvellous@gmail.comm>
2026-07-13 23:12:25 +0100

    Wrap all homepage JSON-LD for Blade

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -79,7 +79,6 @@
       }
     }
     </script>
-    @endverbatim
 
     {{-- Organization Structured Data --}}
     <script type="application/ld+json">
@@ -115,6 +114,7 @@
       }
     }
     </script>
+    @endverbatim
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700,800" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
